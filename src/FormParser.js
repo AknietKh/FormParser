7e9c14a456ecb95aac9feffce25947788fec01a3
Akniet.Kh <akniet.khakimov@mail.ru>
2020-04-03 02:13:18 +0700

    удалил ненужные импорты

--- src/FormParser.js
+++ src/FormParser.js
@@ -1,8 +1,5 @@
-import { SignInForm } from './SignInForm.js';
-import { SignUpForm } from './SignUpForm.js';
-import { ColorShemeForm } from './ColorShemeForm.js';
-
 //принимает инстанс класса формы и рендерит форму (в классах форм должен реализоваться метод renderForm)
+
 class FormParser {
   
   static setForm(form) {
