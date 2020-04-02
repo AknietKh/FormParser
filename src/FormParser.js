ba68a9ad2076d29ae1dee695c3ade95379d8a40a
Akniet.Kh <akniet.khakimov@mail.ru>
2020-04-03 02:11:12 +0700

    Изменил комментарии

--- src/FormParser.js
+++ src/FormParser.js
@@ -2,17 +2,9 @@ import { SignInForm } from './SignInForm.js';
 import { SignUpForm } from './SignUpForm.js';
 import { ColorShemeForm } from './ColorShemeForm.js';
 
-//принимает инстанс класса формы и рендерит форму (в классах форм должен реализоваться метод renderForm)
+//Реализует метод рендера, который принимает тип формы, которую нужено отобразить
 class FormParser {
   
-  // static setForm(form) {
-  //   this.form = form;
-  // }
-
-  // static render() {
-  //   this.form.renderForm();
-  // }
-
   constructor(json) {
     this.formData = json;
   }
