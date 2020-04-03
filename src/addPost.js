4d6b1c933100eb644f753614b29e174549d2955f
Akniet.Kh <akniet.khakimov@mail.ru>
2020-04-04 05:11:53 +0700

    создание различных форм

--- src/addPost.js
+++ src/addPost.js
@@ -1,16 +1,9 @@
-import { ResponseToUrl } from './ResponseToUrl.js';
 import { FormParser } from './FormParser.js';
+import { ResponseToUrl } from './ResponseToUrl.js';
 
 ResponseToUrl.get('./db/addPost.json')
   .then(data => {
-    try {
-      const form = new FormParser(data);
+    const form = new FormParser(data);
 
-      form.renderForm('addPost');
-      
-    } catch(err) {
-      alert(err);
-      console.warn(err);
-    }
-    
-  });
+    form.render();
+  })
--- src/signUp.js
+++ src/signUp.js
@@ -1,15 +1,9 @@
-import { ResponseToUrl } from './ResponseToUrl.js';
 import { FormParser } from './FormParser.js';
+import { ResponseToUrl } from './ResponseToUrl.js';
 
 ResponseToUrl.get('./db/signUp.json')
   .then(data => {
-    try {
-      const form = new FormParser(data);
+    const form = new FormParser(data);
 
-      form.renderForm('signUp');
-      
-    } catch(err) {
-      alert(err);
-      console.warn(err);
-    }
-  });
+    form.render();
+  })
