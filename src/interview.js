226e10a7f48ffffcc0256770c3d71ada9953207d
Akniet.Kh <akniet.khakimov@mail.ru>
2020-04-04 22:35:10 +0700

    Создание формы interview

---
+++ src/interview.js
@@ -0,0 +1,13 @@
+import { FormParser } from './FormParser.js';
+import { ResponseToUrl } from './ResponseToUrl.js';
+
+ResponseToUrl.get('./db/interview.json')
+  .then(data => {
+    const form = new FormParser(data);
+    const formContainer = document.querySelector('.form-container');
+
+    const formTemplate = form.render();
+    console.log(formTemplate);
+
+    formContainer.append(formTemplate);
+  })
